cca89d8851e9758fe3821c1d6f791df8df57aef6
DrizzlyOwl <ashley.davies93@gmail.com>
2021-03-27 21:33:48 +0000

    PSR-12 fixes

--- content/themes/ashdavies/header.php
+++ content/themes/ashdavies/header.php
@@ -6,7 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php wp_title(); ?></title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Ubuntu+Mono&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Ubuntu+Mono&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
     <?php wp_head(); ?>
 </head>
