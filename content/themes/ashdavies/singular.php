069b41e6f5f063c7fbdaff3b11c617fbf2de2abe
DrizzlyOwl <ashley.davies93@gmail.com>
2021-04-15 18:22:45 +0100

    added back button to masthead

--- content/themes/ashdavies/singular.php
+++ content/themes/ashdavies/singular.php
@@ -11,6 +11,7 @@ get_header();
 <main id="contentarea">
     <div class="masthead masthead--slim">
         <header class="masthead__inner">
+            <p class="masthead__back"><a href="/">&larr; Back to Homepage</a></p>
             <h1 class="masthead__heading"><?php the_title(); ?></h1>
             <p class="date"><?php echo get_post_datetime()->format(get_option('date_format')); ?></p>
             <div class="masthead__blurb"><?php the_excerpt(); ?></div>
