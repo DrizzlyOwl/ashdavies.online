d20337a70669a6e0cc38a681700d604a4a8afeef
DrizzlyOwl <ashley.davies93@gmail.com>
2021-06-05 21:42:51 +0100

    locally host google fonts for better CRP

--- content/themes/ashdavies/header.php
+++ content/themes/ashdavies/header.php
@@ -5,9 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?php wp_title(); ?></title>
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display&family=Ubuntu+Mono&display=swap"
-        rel="stylesheet">
     <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
