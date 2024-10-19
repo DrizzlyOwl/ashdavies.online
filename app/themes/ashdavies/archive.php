777ffd75d2adff089992eddf1d1be3b1170f37ce
Ash Davies <3853061+DrizzlyOwl@users.noreply.github.com>
2024-10-19 21:39:06 +0100

    Nicer layout for archive page

--- app/themes/ashdavies/archive.php
+++ app/themes/ashdavies/archive.php
@@ -18,14 +18,15 @@ get_header();
     </div>
     <section class="wrapper">
         <?php if (have_posts()) : ?>
-            <ul class="unset-list">
+            <ul class="cards">
             <?php while (have_posts()) : ?>
                 <?php the_post(); ?>
-                <li <?php post_class("contentarea contentarea--list-item"); ?>>
-                    <h2><?php the_title(); ?></h2>
-                    <p class="date"><?php echo get_post_datetime()->format(get_option('date_format')); ?></p>
-                    <?php the_excerpt(); ?>
-                    <?php the_content(__("Read more&mldr;", 'ashdavies')); ?>
+                <li <?php post_class("card__item"); ?>>
+                    <div class="card__inner">
+                        <p class="date card__meta"><?php echo get_post_datetime()->format(get_option('date_format')); ?></p>
+                        <h2 class="card__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+                        <div class="card__readmore"><?php the_excerpt(); ?></div>
+                    </div>
                 </li>
             <?php endwhile; ?>
             </ul>
